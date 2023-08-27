d3500074edd66e509173050040547ac405d9f007
Abhishek6973 <108926573+Abhishek6973@users.noreply.github.com>
2023-08-27 12:12:39 +0530

    Update httpsSendReq.py

--- httpsSendReq.py
+++ httpsSendReq.py
@@ -3,7 +3,7 @@ import time
 import os
 
 if __name__ == "__main__":
-    target_url = "https://formatconverterbot-multifunctional-tg-ak.onrender.com/5504002265:AAG1pUCPJXiU9N6-r0-fOEnaJ_cZ9wZQ3Dc"
+    target_url = os.getenv("LIVE_URL")
     no = 1
     while True:
         requests.get(target_url)
