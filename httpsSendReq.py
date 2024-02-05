f7b0630ef694295f8ad263bfd93aae2c78397555
Abhishek6973 <108926573+Abhishek6973@users.noreply.github.com>
2024-02-05 16:10:53 +0530

    Update httpsSendReq.py

--- httpsSendReq.py
+++ httpsSendReq.py
@@ -4,9 +4,11 @@ import os
 
 if __name__ == "__main__":
     target_url = os.getenv("LIVE_URL")
+    next_target_url = os.getenv("SEC_LIVE_URL")
     no = 1
     while True:
         requests.get(target_url)
+        requests.get(next_target_url)
         print("Success No:-",no)
         no += 1
         time.sleep(50)  # Sleep for 60 seconds (1 minute)
